9d5cb0aea6e152f0587488aa4c001628141954f6
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-07-06 12:44:13 -0400

    db setup

---
+++ contexts/@projects/infra/.config/kysely.config.ts
@@ -0,0 +1,15 @@
+
+import { defineConfig } from 'kysely-ctl'
+import SQLite from 'better-sqlite3'
+import { SqliteDialect } from 'kysely'
+
+const dialect = new SqliteDialect({
+	database: new SQLite(process.env.PROJECT_DB),
+})
+
+export default defineConfig({
+	dialect,
+	migrations: {
+		migrationFolder: './persistence/migrations'
+	}
+});
