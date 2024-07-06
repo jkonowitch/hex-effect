c5ff873ff6d45bc9169cf5e090aa0dec722f4bfd
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-07-06 11:09:22 -0400

    application service

--- contexts/@projects/application/index.ts
+++ contexts/@projects/application/index.ts
@@ -1,6 +1,5 @@
 import { Schema } from "@effect/schema";
 import { Project, ProjectId, ProjectRepository, TaskId } from "@projects/domain";
-import { Router, Rpc } from "@effect/rpc"
 import { Effect } from "effect";
 
 /**
@@ -20,12 +19,10 @@ export class AddTask extends Schema.TaggedRequest<AddTask>()("AddTask", Schema.N
  * Application Services
  */
 
-const createProject = Rpc.effect(CreateProject, ({ title }) => Project.create(title).pipe(Effect.map(project => project.id)));
+export const createProject = ({ title }: CreateProject) => Project.create(title).pipe(Effect.map(project => project.id));
 
-const addTask = Rpc.effect(AddTask, ({ description, projectId }) => Effect.gen(function* () {
+export const addTask = ({ description, projectId }: AddTask) => Effect.gen(function* () {
   const project = yield* Effect.serviceFunctions(ProjectRepository).findById(projectId);
   const task = yield* project.addTask(description);
   return task.id
-}));
-
-export const router = Router.make(createProject, addTask);
+});
