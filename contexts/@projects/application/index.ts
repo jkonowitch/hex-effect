7f7d290e156b0da59d0ee88b7fb390f6f7295b12
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-07-06 11:31:52 -0400

    complete task service

--- contexts/@projects/application/index.ts
+++ contexts/@projects/application/index.ts
@@ -1,5 +1,5 @@
 import { Schema } from "@effect/schema";
-import { Project, ProjectId, ProjectRepository, TaskId } from "@projects/domain";
+import { Project, ProjectId, ProjectRepository, Task, TaskId, TaskRepository } from "@projects/domain";
 import { Effect, type Request } from "effect";
 
 /**
@@ -15,16 +15,31 @@ export class AddTask extends Schema.TaggedRequest<AddTask>()("AddTask", Schema.N
   projectId: ProjectId
 }) { }
 
+export class CompleteTask extends Schema.TaggedRequest<CompleteTask>()("CompleteTask", Schema.Never, Schema.Void, {
+  taskId: TaskId
+}) { }
+
 /**
  * Application Services
  */
 
 type RequestHandler<A extends Request.Request<unknown, unknown>> = Effect.Effect<Request.Request.Success<A>, Request.Request.Error<A>, unknown>
 
-export const createProject = ({ title }: CreateProject) => Project.create(title).pipe(Effect.map(project => project.id)) satisfies RequestHandler<CreateProject>;
+export const createProject = ({ title }: CreateProject) => Effect.gen(function* () {
+  const project = yield* Project.create(title);
+  yield* Effect.serviceFunctions(ProjectRepository).save(project)
+  return project.id
+}) satisfies RequestHandler<CreateProject>;
 
 export const addTask = ({ description, projectId }: AddTask) => Effect.gen(function* () {
   const project = yield* Effect.serviceFunctions(ProjectRepository).findById(projectId);
   const task = yield* project.addTask(description);
+  yield* Effect.serviceFunctions(TaskRepository).save(task)
   return task.id
 }) satisfies RequestHandler<AddTask>;
+
+export const completeTask = ({ taskId }: CompleteTask) => Effect.gen(function* () {
+  const repo = yield* TaskRepository;
+  const task = yield* repo.findById(taskId).pipe(Effect.flatMap(Task.complete));
+  repo.save(task)
+}) satisfies RequestHandler<CompleteTask>
