84fdd34a6bb2b7c96312620bf6d6a40576e236c1
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-07-07 12:52:28 -0400

    succeedOrNotFound!

--- contexts/@projects/application/index.ts
+++ contexts/@projects/application/index.ts
@@ -7,11 +7,15 @@ import {
   TaskId,
   TaskRepository
 } from '@projects/domain';
-import { Effect, type Request } from 'effect';
+import { Effect, type Request, Option, pipe } from 'effect';
 
 /**
  * Requests
  */
+
+export class ApplicationError extends Schema.TaggedError<ApplicationError>()('ApplicationError', {
+  message: Schema.String
+}) {}
 
 export class CreateProject extends Schema.TaggedRequest<CreateProject>()(
   'CreateProject',
@@ -22,14 +26,14 @@ export class CreateProject extends Schema.TaggedRequest<CreateProject>()(
   }
 ) {}
 
-export class AddTask extends Schema.TaggedRequest<AddTask>()('AddTask', Schema.Never, TaskId, {
+export class AddTask extends Schema.TaggedRequest<AddTask>()('AddTask', ApplicationError, TaskId, {
   description: Schema.String,
   projectId: ProjectId
 }) {}
 
 export class CompleteTask extends Schema.TaggedRequest<CompleteTask>()(
   'CompleteTask',
-  Schema.Never,
+  ApplicationError,
   Schema.Void,
   {
     taskId: TaskId
@@ -55,7 +59,10 @@ export const createProject = ({ title }: CreateProject) =>
 
 export const addTask = ({ description, projectId }: AddTask) =>
   Effect.gen(function* () {
-    const project = yield* Effect.serviceFunctions(ProjectRepository).findById(projectId);
+    const project = yield* pipe(
+      Effect.serviceFunctions(ProjectRepository).findById(projectId),
+      succeedOrNotFound(`No project ${projectId}`)
+    );
     const task = yield* project.addTask(description);
     yield* Effect.serviceFunctions(TaskRepository).save(task);
     return task.id;
@@ -64,6 +71,26 @@ export const addTask = ({ description, projectId }: AddTask) =>
 export const completeTask = ({ taskId }: CompleteTask) =>
   Effect.gen(function* () {
     const repo = yield* TaskRepository;
-    const task = yield* repo.findById(taskId).pipe(Effect.flatMap(Task.complete));
+    const task = yield* pipe(
+      repo.findById(taskId),
+      succeedOrNotFound(`No task ${taskId}`),
+      Effect.flatMap(Task.complete)
+    );
     repo.save(task);
   }) satisfies RequestHandler<CompleteTask>;
+
+/**
+ * Utils
+ */
+
+function succeedOrNotFound<A, R>(message = 'Not Found') {
+  return (eff: Effect.Effect<Option.Option<A>, never, R>) =>
+    eff.pipe(
+      Effect.flatMap(
+        Option.match({
+          onNone: () => new ApplicationError({ message }),
+          onSome: Effect.succeed
+        })
+      )
+    );
+}
