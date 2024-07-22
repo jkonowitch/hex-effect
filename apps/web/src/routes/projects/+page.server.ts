f7e767b6a709538c34db95b8813e5608017dce50
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-07-22 13:50:06 -0400

    create projects

--- apps/web/src/routes/projects/+page.server.ts
+++ apps/web/src/routes/projects/+page.server.ts
@@ -1,8 +1,9 @@
 import { run, undecodedHandler } from '$lib/server';
-import { GetAllProjects } from '@projects/application';
-import type { PageServerLoad } from './$types';
-import { Cause, Exit } from 'effect';
-import { error } from '@sveltejs/kit';
+import { CreateProject, GetAllProjects } from '@projects/application';
+import type { PageServerLoad, Actions } from './$types';
+import { Cause, Either, Exit } from 'effect';
+import { error, fail } from '@sveltejs/kit';
+import { ArrayFormatter, Schema } from '@effect/schema';
 
 export const load = (async () => {
   const res = await undecodedHandler(new GetAllProjects()).pipe(run);
@@ -12,3 +13,25 @@ export const load = (async () => {
     onFailure: (cause) => (Cause.isFailType(cause) ? error(404, cause.error.message) : error(500))
   });
 }) satisfies PageServerLoad;
+
+export const actions = {
+  createProject: async ({ request }) => {
+    const data = await request.formData();
+    const command = Schema.decodeUnknownEither(CreateProject)(
+      {
+        title: data.get('title')?.toString(),
+        _tag: 'CreateProject'
+      },
+      { onExcessProperty: 'error', errors: 'all' }
+    );
+
+    return Either.match(command, {
+      onLeft: (e) => fail(400, { errors: ArrayFormatter.formatErrorSync(e) }),
+      onRight: async (a) => {
+        await undecodedHandler(a).pipe(run);
+
+        return { success: true };
+      }
+    });
+  }
+} satisfies Actions;
