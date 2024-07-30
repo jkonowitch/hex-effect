b1e9d902d87b97f58792bbfecd59ad76f4a3db42
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-07-30 15:17:36 -0400

    slim down infra for now

--- examples/todo-app/contexts/@projects/infra/index.ts
+++ examples/todo-app/contexts/@projects/infra/index.ts
@@ -1,26 +1,25 @@
-import { Effect, Layer, Logger, LogLevel, ManagedRuntime } from 'effect';
-import { registerEvents, EventHandlerService } from '@projects/application';
-import { makeEventHandlerService, WithoutDependencies } from '@hex-effect/infra-kysely-libsql-nats';
-import { NatsService } from './services.js';
+import { Layer, Logger, LogLevel, ManagedRuntime } from 'effect';
+import { WithoutDependencies } from '@hex-effect/infra-kysely-libsql-nats';
+// import { NatsService } from './services.js';
 import { DomainServiceLive } from './repositories.js';
 
-const EventHandlerLive = NatsService.pipe(
-  Effect.andThen((nats) => makeEventHandlerService(nats, EventHandlerService))
-).pipe(Layer.unwrapEffect);
+// const EventHandlerLive = NatsService.pipe(
+//   Effect.andThen((nats) => makeEventHandlerService(nats, EventHandlerService))
+// ).pipe(Layer.unwrapEffect);
 
-const EventDaemonLive = Layer.effectDiscard(
-  registerEvents.pipe(Effect.provide(DomainServiceLive), Effect.forkDaemon)
-);
+// const EventDaemonLive = Layer.effectDiscard(
+//   registerEvents.pipe(Effect.provide(DomainServiceLive), Effect.forkDaemon)
+// );
 
-const InfrastructureLive = EventDaemonLive.pipe(
-  Layer.provideMerge(EventHandlerLive),
-  Layer.provide(NatsService.live),
-  Layer.provideMerge(WithoutDependencies)
-);
+// const InfrastructureLive = EventDaemonLive.pipe(
+//   Layer.provideMerge(EventHandlerLive),
+//   Layer.provide(NatsService.live),
+//   Layer.provideMerge(WithoutDependencies)
+// );
 
 export const managedRuntime = ManagedRuntime.make(
   DomainServiceLive.pipe(
-    Layer.provideMerge(InfrastructureLive),
+    Layer.provideMerge(WithoutDependencies),
     Layer.provide(Logger.minimumLogLevel(LogLevel.All)),
     Layer.orDie
   )
