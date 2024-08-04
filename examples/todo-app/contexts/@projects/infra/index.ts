38f955e7aec126484d2022d1014af2f1f15a87de
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-08-03 21:37:57 -0400

    layerzzz!

--- examples/todo-app/contexts/@projects/infra/index.ts
+++ examples/todo-app/contexts/@projects/infra/index.ts
@@ -1,24 +1,15 @@
-import { Layer, Logger, LogLevel, ManagedRuntime } from 'effect';
+import { Effect, Layer, Logger, LogLevel, ManagedRuntime } from 'effect';
 import { InfrastructureLayer } from '@hex-effect/infra-kysely-libsql-nats';
-// import { NatsService } from './services.js';
 import { DomainServiceLive } from './repositories.js';
+import { CreateProject, registerEvents, router } from '@projects/application';
+import { Router } from '@effect/rpc';
 
-// const EventHandlerLive = NatsService.pipe(
-//   Effect.andThen((nats) => makeEventHandlerService(nats, EventHandlerService))
-// ).pipe(Layer.unwrapEffect);
-
-// const EventDaemonLive = Layer.effectDiscard(
-//   registerEvents.pipe(Effect.provide(DomainServiceLive), Effect.forkDaemon)
-// );
-
-// const InfrastructureLive = EventDaemonLive.pipe(
-//   Layer.provideMerge(EventHandlerLive),
-//   Layer.provide(NatsService.live),
-//   Layer.provideMerge(WithoutDependencies)
-// );
+const EventDaemonLive = Layer.effectDiscard(registerEvents.pipe(Effect.forkDaemon));
 
 export const managedRuntime = ManagedRuntime.make(
-  DomainServiceLive.pipe(
+  Layer.empty.pipe(
+    Layer.provide(EventDaemonLive),
+    Layer.provideMerge(DomainServiceLive),
     Layer.provideMerge(InfrastructureLayer),
     Layer.provide(Logger.minimumLogLevel(LogLevel.All)),
     Layer.orDie
