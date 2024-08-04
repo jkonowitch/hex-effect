aa9a252903ca0e6fc76cc5bcbe8f10c0fd7fb4c9
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-08-03 21:38:30 -0400

    clean up

--- examples/todo-app/contexts/@projects/infra/index.ts
+++ examples/todo-app/contexts/@projects/infra/index.ts
@@ -1,8 +1,7 @@
 import { Effect, Layer, Logger, LogLevel, ManagedRuntime } from 'effect';
 import { InfrastructureLayer } from '@hex-effect/infra-kysely-libsql-nats';
 import { DomainServiceLive } from './repositories.js';
-import { CreateProject, registerEvents, router } from '@projects/application';
-import { Router } from '@effect/rpc';
+import { registerEvents } from '@projects/application';
 
 const EventDaemonLive = Layer.effectDiscard(registerEvents.pipe(Effect.forkDaemon));
 
