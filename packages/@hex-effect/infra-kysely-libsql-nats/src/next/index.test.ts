82944c6046c04628a212b16db3fe7baa689c66fe
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-04 12:46:25 -0400

    figured out domain model / infra model division

--- packages/@hex-effect/infra-kysely-libsql-nats/src/next/index.test.ts
+++ packages/@hex-effect/infra-kysely-libsql-nats/src/next/index.test.ts
@@ -1,11 +1,12 @@
 import { Model, SqlClient, SqlError } from '@effect/sql';
 import { LibsqlClient } from './libsql-client/index.js';
 import { describe, expect, layer } from '@effect/vitest';
-import { Effect, Config, Context, Layer } from 'effect';
+import { Effect, Config, Context, Layer, String, Console } from 'effect';
 import { GenericContainer, type StartedTestContainer } from 'testcontainers';
 import { Schema, Serializable } from '@effect/schema';
 import { EventBaseSchema } from '@hex-effect/core';
 import { nanoid } from 'nanoid';
+import type { ParseError } from '@effect/schema/ParseResult';
 
 export class LibsqlContainer extends Context.Tag('test/LibsqlContainer')<
   LibsqlContainer,
@@ -30,7 +31,9 @@ export class LibsqlContainer extends Context.Tag('test/LibsqlContainer')<
       const container = yield* LibsqlContainer;
       return LibsqlClient.layer({
         _tag: Config.succeed('Config'),
-        url: Config.succeed(`http://localhost:${container.getMappedPort(8080)}`)
+        url: Config.succeed(`http://localhost:${container.getMappedPort(8080)}`),
+        transformQueryNames: Config.succeed(String.camelToSnake),
+        transformResultNames: Config.succeed(String.snakeToCamel)
       });
     })
   ).pipe(Layer.provide(this.Live));
@@ -58,28 +61,43 @@ export class PersonCreatedEvent extends Schema.TaggedClass<PersonCreatedEvent>()
 
 const PersonId = Schema.NonEmptyTrimmedString.pipe(Schema.brand('PersonId'));
 
-class PersonModel extends Model.Class<PersonModel>('PersonModel')({
+const PersonDomainModel = Schema.Struct({
+  id: PersonId,
+  name: Schema.Trim.pipe(Schema.compose(Schema.NonEmptyString))
+});
+
+class PersonSQLModel extends Model.Class<PersonSQLModel>('PersonSQLModel')({
+  ...PersonDomainModel.fields,
   id: Model.GeneratedByApp(PersonId),
-  name: Schema.Trim
+  createdAt: Model.DateTimeInsertFromNumber,
+  updatedAt: Model.DateTimeUpdateFromNumber
 }) {}
 
 class SavePerson extends Context.Tag('test/SavePerson')<
   SavePerson,
-  (person: typeof PersonModel.insert.Type) => Effect.Effect<void, SqlError.SqlError>
+  (person: typeof PersonDomainModel.Type) => Effect.Effect<void, SqlError.SqlError | ParseError>
 >() {
   public static live = Layer.effect(
     this,
     Effect.gen(function* () {
       const sql = yield* SqlClient.SqlClient;
-
-      return (person) => sql`insert into people ${sql.insert(person)};`;
+      return (person) =>
+        Effect.gen(function* () {
+          const insert = yield* Schema.encode(PersonSQLModel.insert)(
+            PersonSQLModel.insert.make(person)
+          );
+          yield* sql`insert into people ${sql.insert(insert)};`;
+        });
     })
   );
 }
 
-const addPerson = (p: typeof PersonModel.jsonCreate.Type) =>
+const addPerson = (name: string) =>
   Effect.gen(function* () {
-    const person = yield* Schema.decode(PersonModel.insert)({ ...p, id: PersonId.make(nanoid()) });
+    const person = yield* Schema.decode(PersonDomainModel)({
+      name,
+      id: PersonId.make(nanoid())
+    });
     const save = yield* SavePerson;
     yield* save(person);
     return [PersonCreatedEvent.make({ id: person.id })];
@@ -89,7 +107,7 @@ const Migrations = Layer.scopedDiscard(
   Effect.gen(function* () {
     const sql = yield* SqlClient.SqlClient;
     yield* Effect.acquireRelease(
-      sql`create table people (id text primary key not null, name text not null);`,
+      sql`create table people (id text primary key not null, name text not null, created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);`,
       () => sql`drop table people;`.pipe(Effect.ignore)
     );
   })
@@ -102,10 +120,11 @@ describe('kralf', () => {
     it.scoped('does a thing', () =>
       Effect.gen(function* () {
         const sql = yield* SqlClient.SqlClient;
-        const events = yield* addPerson({ name: 'Jeffrey ' });
+        const events = yield* addPerson('Jeffrey ');
         const res = yield* sql<{
           name: string;
-        }>`select name from people where id = ${events.at(0)!.id};`;
+        }>`select * from people where id = ${events.at(0)!.id};`;
+        yield* Console.log(res);
         expect(res.at(0)!.name).toEqual('Jeffrey');
       }).pipe(Effect.provide(Migrations))
     );
