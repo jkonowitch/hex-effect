73bd91cf0e2a2fb87e5072adacc69dcefd0a3bf8
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-07 15:18:58 -0400

    eventstore

--- packages/@hex-effect/infra-kysely-libsql-nats/src/next/index.ts
+++ packages/@hex-effect/infra-kysely-libsql-nats/src/next/index.ts
@@ -5,10 +5,12 @@ import {
   type EventBaseType
 } from '@hex-effect/core';
 import { Context, Effect, Layer, Option, Ref } from 'effect';
-import type { Statement } from '@effect/sql';
+import { Model, type Statement } from '@effect/sql';
 import type { SqlError } from '@effect/sql/SqlError';
 import { LibsqlClient } from './libsql-client/index.js';
 import type { InValue } from '@libsql/client';
+import type { ParseError } from '@effect/schema/ParseResult';
+import { Schema } from '@effect/schema';
 
 class WriteExecutor extends Context.Tag('WriteExecutor')<
   WriteExecutor,
@@ -27,6 +29,64 @@ export class WriteStatement extends Context.Tag('WriteStatement')<
       Effect.andThen((wr) => wr(stm))
     )
   ).pipe(Layer.provideMerge(WriteExecutor.live));
+}
+
+const BoolFromNumber = Schema.transform(Schema.Number, Schema.Boolean, {
+  strict: true,
+  decode: (fromA) => (fromA === 0 ? false : true),
+  encode: (toI) => (toI ? 1 : 0)
+});
+
+class EventModel extends Model.Class<EventModel>('EventModel')({
+  messageId: Schema.String,
+  occurredOn: Schema.DateFromNumber,
+  delivered: BoolFromNumber,
+  payload: Schema.String
+}) {}
+
+class EventStore extends Context.Tag('@hex-effect/libsql/event-store')<
+  EventStore,
+  {
+    save: (e: EventBaseType[]) => Effect.Effect<void, ParseError | SqlError>;
+  }
+>() {
+  public static live = Layer.effect(
+    this,
+    Effect.gen(function* () {
+      const sql = yield* LibsqlClient.LibsqlClient;
+      const writer = yield* WriteStatement;
+      const [ensureEventTableStmt] = sql`CREATE TABLE IF NOT EXISTS hex_effect_events (
+        message_id TEXT PRIMARY KEY NOT NULL,
+        occurred_on DATETIME NOT NULL,
+        delivered INTEGER NOT NULL DEFAULT 0,
+        payload TEXT NOT NULL
+      );`.compile();
+
+      yield* Effect.promise(() => sql.sdk.migrate([{ sql: ensureEventTableStmt, args: [] }]));
+
+      return {
+        save: (events) =>
+          Effect.forEach(
+            events,
+            (e) =>
+              e.encode().pipe(
+                Effect.flatMap((e) =>
+                  Schema.encode(EventModel)({
+                    delivered: false,
+                    messageId: e.messageId,
+                    occurredOn: Schema.decodeSync(Schema.DateFromString)(e.occurredOn),
+                    payload: JSON.stringify(e)
+                  })
+                ),
+                Effect.tap((e) => writer(sql`insert into events ${sql.insert(e)};`))
+              ),
+            {
+              concurrency: 'unbounded'
+            }
+          )
+      };
+    })
+  );
 }
 
 export const WTLive = Layer.effect(
