85ea851dc0ad0f7ea2471e20ca49ecd02b4ceac6
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-12 13:46:03 -0400

    refine outputs

--- packages/@hex-effect/infra-kysely-libsql-nats/src/next/index.ts
+++ packages/@hex-effect/infra-kysely-libsql-nats/src/next/index.ts
@@ -1,11 +1,13 @@
 import { EventConsumer } from '@hex-effect/core';
 import { Context, Layer } from 'effect';
 import { NatsEventConsumer } from './messaging.js';
+import { WithTransactionLive } from './transactional-boundary.js';
 
-export const EventConsumerLive = Layer.map(NatsEventConsumer.Default, (ctx) => {
+const EventConsumerLive = Layer.map(NatsEventConsumer.Default, (ctx) => {
   const service = Context.get(ctx, NatsEventConsumer);
   return ctx.pipe(Context.omit(NatsEventConsumer), Context.add(EventConsumer, service));
 });
+
 export { NatsClient } from './messaging.js';
 export { LibsqlSdk, LibsqlConfig } from './sql.js';
-export { WithTransactionLive } from './transactional-boundary.js';
+export const Live = Layer.merge(EventConsumerLive, WithTransactionLive);
