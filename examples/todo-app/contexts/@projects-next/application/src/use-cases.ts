ab4effd43bdaf273ff1e0d85baf7e67e02eb517f
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-14 18:14:44 -0400

    small refactor

--- examples/todo-app/contexts/@projects-next/application/src/use-cases.ts
+++ examples/todo-app/contexts/@projects-next/application/src/use-cases.ts
@@ -1,20 +1,21 @@
 import { Project, Task } from '@projects-next/domain';
-import { Effect } from 'effect';
+import { Effect, Option } from 'effect';
 import { FindProjectById, SaveProject, SaveTask } from './services.js';
 import { IsolationLevel, withTXBoundary } from '@hex-effect/core';
 
-export const createProject = Effect.gen(function* () {
-  const [project, event] = yield* Project.Service.createProject('hello');
-  yield* Effect.serviceFunctions(SaveProject).save(project);
-  return [event];
-}).pipe(withTXBoundary(IsolationLevel.Batched));
+export const createProject = (title: string) =>
+  Effect.gen(function* () {
+    const [project, event] = yield* Project.Service.createProject(title);
+    yield* Effect.serviceFunctions(SaveProject).save(project);
+    return [event];
+  }).pipe(withTXBoundary(IsolationLevel.Batched));
 
 export const addTaskToProject = (params: { projectId: string; description: string }) =>
   Effect.gen(function* () {
     const project = yield* Effect.serviceFunctions(FindProjectById).findById(
       Project.Model.ProjectId.make(params.projectId)
     );
-    if (project._tag === 'None') {
+    if (Option.isNone(project)) {
       return yield* Effect.fail('Not found');
     } else {
       const [task, event] = yield* Task.Service.addTaskToProject(project.value, params.description);
