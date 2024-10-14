345ae3832d308fe26e514afa0153af444615dd82
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-14 16:13:37 -0400

    domain model services now generate events

--- examples/todo-app/contexts/@projects-next/core/src/model/project/service.ts
+++ examples/todo-app/contexts/@projects-next/core/src/model/project/service.ts
@@ -2,14 +2,9 @@ import { Effect } from 'effect';
 import { Project, ProjectCreatedEvent, ProjectId } from './model.js';
 import { UUIDGenerator } from '@hex-effect/core';
 
-export const createProject = (
-  title: string
-): Effect.Effect<
-  [typeof Project.Type, ReturnType<typeof ProjectCreatedEvent.make>],
-  never,
-  UUIDGenerator
-> =>
-  UUIDGenerator.generate().pipe(
-    Effect.map((uuid) => Project.make({ title, id: ProjectId.make(uuid) })),
-    Effect.map((project) => [project, ProjectCreatedEvent.make({ id: project.id })] as const)
-  );
+export const createProject = (title: string) =>
+  Effect.gen(function* () {
+    const project = Project.make({ title, id: ProjectId.make(yield* UUIDGenerator.generate()) });
+    const event = yield* ProjectCreatedEvent.make({ id: project.id });
+    return [project, event] as const;
+  });
--- examples/todo-app/contexts/@projects-next/core/src/model/task/service.ts
+++ examples/todo-app/contexts/@projects-next/core/src/model/task/service.ts
@@ -1,33 +1,21 @@
 import { Effect } from 'effect';
+import { UUIDGenerator } from '@hex-effect/core';
 import { Task, TaskAddedEvent, TaskCompletedEvent, TaskId } from './model.js';
 import type { Project } from '../project/model.js';
-import { UUIDGenerator } from '@hex-effect/core';
 
 const makeWithUUID = (args: Omit<Parameters<(typeof Task)['make']>[0], 'id'>) =>
   UUIDGenerator.generate().pipe(
     Effect.map((uuid) => Task.make({ ...args, id: TaskId.make(uuid) }))
   );
 
-export const addTaskToProject = (
-  project: typeof Project.Type,
-  description: string
-): Effect.Effect<
-  [typeof Task.Type, ReturnType<typeof TaskAddedEvent.make>],
-  never,
-  UUIDGenerator
-> =>
-  makeWithUUID({ projectId: project.id, description }).pipe(
-    Effect.map(
-      (task) => [task, TaskAddedEvent.make({ projectId: task.projectId, taskId: task.id })] as const
-    )
-  );
+export const addTaskToProject = (project: typeof Project.Type, description: string) =>
+  Effect.gen(function* () {
+    const task = yield* makeWithUUID({ description, projectId: project.id });
+    return [task, yield* TaskAddedEvent.make({ taskId: task.id, projectId: project.id })] as const;
+  });
 
-export const complete = (
-  task: typeof Task.Type
-): [typeof Task.Type, ReturnType<(typeof TaskCompletedEvent)['make']>] => [
-  {
-    ...task,
-    completed: true
-  },
-  TaskCompletedEvent.make({ taskId: task.id })
-];
+export const complete = (task: typeof Task.Type) =>
+  Effect.gen(function* () {
+    const completedTask: typeof Task.Type = { ...task, completed: true };
+    return [completedTask, yield* TaskCompletedEvent.make({ taskId: task.id })] as const;
+  });
