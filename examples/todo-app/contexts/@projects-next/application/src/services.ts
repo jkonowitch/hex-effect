05cc5a6f5406ffc3fa62ff9b89c38ab9fa2b93ca
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-14 17:52:43 -0400

    another use case

--- examples/todo-app/contexts/@projects-next/application/src/services.ts
+++ examples/todo-app/contexts/@projects-next/application/src/services.ts
@@ -1,7 +1,23 @@
-import { Context, Effect } from 'effect';
-import { Project } from '@projects-next/domain';
+import { Context, Effect, Option } from 'effect';
+import { Project, Task } from '@projects-next/domain';
 
 export class SaveProject extends Context.Tag('@projects/application/SaveProject')<
   SaveProject,
   { save: (p: typeof Project.Model.Project.Type) => Effect.Effect<void> }
+>() {}
+
+export class FindProjectById extends Context.Tag('@projects/application/FindProjectById')<
+  FindProjectById,
+  {
+    findById: (
+      p: typeof Project.Model.ProjectId.Type
+    ) => Effect.Effect<Option.Option<typeof Project.Model.Project.Type>>;
+  }
+>() {}
+
+export class SaveTask extends Context.Tag('@projects/application/SaveTask')<
+  SaveTask,
+  {
+    save: (p: typeof Task.Model.Task.Type) => Effect.Effect<void>;
+  }
 >() {}
--- examples/todo-app/contexts/@projects-next/application/src/use-cases.ts
+++ examples/todo-app/contexts/@projects-next/application/src/use-cases.ts
@@ -1,6 +1,6 @@
-import { Project } from '@projects-next/domain';
+import { Project, Task } from '@projects-next/domain';
 import { Effect } from 'effect';
-import { SaveProject } from './services.js';
+import { FindProjectById, SaveProject, SaveTask } from './services.js';
 import { IsolationLevel, withTXBoundary } from '@hex-effect/core';
 
 export const createProject = Effect.gen(function* () {
@@ -8,3 +8,17 @@ export const createProject = Effect.gen(function* () {
   yield* Effect.serviceFunctions(SaveProject).save(project);
   return [event];
 }).pipe(withTXBoundary(IsolationLevel.Batched));
+
+export const addTaskToProject = (params: { projectId: string; description: string }) =>
+  Effect.gen(function* () {
+    const project = yield* Effect.serviceFunctions(FindProjectById).findById(
+      Project.Model.ProjectId.make(params.projectId)
+    );
+    if (project._tag === 'None') {
+      return yield* Effect.fail('Not found');
+    } else {
+      const [task, event] = yield* Task.Service.addTaskToProject(project.value, params.description);
+      yield* Effect.serviceFunctions(SaveTask).save(task);
+      return [event];
+    }
+  }).pipe(withTXBoundary(IsolationLevel.Batched));
