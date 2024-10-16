329e787d93d8a3806e6babbed0434325e9d47d2b
Jeffrey Konowitch <jkonowitch@gmail.com>
2024-10-16 15:25:26 -0400

    just get the basic use case working

--- examples/todo-app/web/src/hooks.server.ts
+++ examples/todo-app/web/src/hooks.server.ts
@@ -3,10 +3,12 @@ import type { Handle } from '@sveltejs/kit';
 import { ManagedRuntime } from 'effect';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (!event.platform?.runtime) {
+  if (!event.platform) {
     const runtime: globalThis.App.Platform = { runtime: ManagedRuntime.make(Live) };
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (event.platform as any).runtime = runtime;
+    event.platform = runtime;
+    process.on('sveltekit:shutdown', async () => {
+      await runtime.runtime.dispose();
+    });
   }
   const response = await resolve(event);
   return response;
--- examples/todo-app/web/src/routes/projects/+page.server.ts
+++ examples/todo-app/web/src/routes/projects/+page.server.ts
@@ -1,38 +1,34 @@
-import { undecodedHandler } from '$lib/server';
-import { CreateProject, GetAllProjects } from '@projects/application';
-import type { PageServerLoad, Actions } from './$types';
-import { Cause, Either, Exit } from 'effect';
-import { error, fail } from '@sveltejs/kit';
-import { ArrayFormatter, Schema } from '@effect/schema';
-import { managedRuntime } from '@projects/infra';
+import type { PageServerLoad } from './$types';
+import { Effect } from 'effect';
+import { UseCases } from '@projects-next/application';
+import { ServiceLive } from '@projects-next/infra';
 
-export const load = (async () => {
-  const res = await undecodedHandler(new GetAllProjects()).pipe(managedRuntime.runPromiseExit);
+export const load = (async ({ platform }) => {
+  const projects = await Effect.runPromise(
+    UseCases.getAllProjects.pipe(Effect.provide(ServiceLive), Effect.provide(platform!.runtime))
+  );
 
-  return Exit.match(res, {
-    onSuccess: (data) => ({ data }),
-    onFailure: (cause) => (Cause.isFailType(cause) ? error(404, cause.error.message) : error(500))
-  });
+  return { projects: projects };
 }) satisfies PageServerLoad;
 
-export const actions = {
-  createProject: async ({ request }) => {
-    const data = await request.formData();
-    const command = Schema.decodeUnknownEither(CreateProject)(
-      {
-        title: data.get('title')?.toString(),
-        _tag: 'CreateProject'
-      },
-      { onExcessProperty: 'error', errors: 'all' }
-    );
+// export const actions = {
+//   createProject: async ({ request }) => {
+//     const data = await request.formData();
+//     const command = Schema.decodeUnknownEither(CreateProject)(
+//       {
+//         title: data.get('title')?.toString(),
+//         _tag: 'CreateProject'
+//       },
+//       { onExcessProperty: 'error', errors: 'all' }
+//     );
 
-    return Either.match(command, {
-      onLeft: (e) => fail(400, { errors: ArrayFormatter.formatErrorSync(e) }),
-      onRight: async (a) => {
-        await undecodedHandler(a).pipe(managedRuntime.runPromise);
+//     return Either.match(command, {
+//       onLeft: (e) => fail(400, { errors: ArrayFormatter.formatErrorSync(e) }),
+//       onRight: async (a) => {
+//         await undecodedHandler(a).pipe(managedRuntime.runPromise);
 
-        return { success: true };
-      }
-    });
-  }
-} satisfies Actions;
+//         return { success: true };
+//       }
+//     });
+//   }
+// } satisfies Actions;
